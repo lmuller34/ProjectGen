43f9c33365ddbb16406eebdd46263c92b2cfae50
Jerome <le.jitou@gmail.com>
2012-10-04 12:02:45 +0200

    Idente

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-	
 
-public class Main{
+public class Main {
 
-public static void main(String[] args){
-    Projet p = new Projet ("Test");
-    p.rajouterTache(new Tache("Tache 1", "Je suis la tache 1"));
-    System.out.println(p);
-}
-
+    public static void main(String[] args) {
+        Projet p = new Projet("Test");
+        p.rajouterTache(new Tache("Tache 1", "Je suis la tache 1"));
+        System.out.println(p);
+    }
 }
