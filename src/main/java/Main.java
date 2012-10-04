dfc5c6254d61d4ca2d0e1bb37d06a5344bee5cda
Jerome <le.jitou@gmail.com>
2012-10-04 11:52:48 +0200

    Code source

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+	
+
+public class Main{
+
+public static void main(String[] args){
+    Projet p = new Projet ("Test");
+    p.rajouterTache(new Tache("Tache 1", "Je suis la tache 1"));
+    System.out.println(p);
+}
+
+}
---
+++ src/main/java/Projet.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+public class Projet {
+
+    private ArrayList<Tache> taches = new ArrayList<Tache>();
+    private String nomProjet;
+
+    Projet(String nomProjet) {
+        this.nomProjet = nomProjet;
+        taches.clear();
+    }
+
+    void rajouterTache(Tache tache) {
+        taches.add(tache);
+    }
+
+    void supprimerTache(Tache tache) {
+        taches.remove(tache);
+    }
+
+    void supprimerTache(int id) {
+        for (int i = 0; i < taches.size(); i++) {
+            if (taches.get(i).getId() == id) {
+                taches.remove(i);
+            }
+        }
+    }
+
+    public String toString() {
+        String tmp = "";
+        tmp += nomProjet + "\n";
+        for (int i = 0; i < taches.size(); i++) {
+            tmp += taches.get(i) + "\n";
+        }
+        return tmp;
+    }
+}
+
