8d09705ec500d560d284edde4d9ffe089afa31ed
jitou <jitou@jitou-Aspire-M5910.(none)>
2012-10-10 22:27:05 +0200

    les dates sont maintenant en relation directe avec le timestamp

--- src/main/java/Projet/Main.java
+++ src/main/java/Projet/Main.java
@@ -2,15 +2,20 @@ package Projet;
 
 public class Main {
 
+    
+    
     public static void main(String[] args) {
+        long JOUR = 3600*24;
+        long MAINTENANT = System.currentTimeMillis()/1000;
+        
         Projet p = new Projet("Test");
         System.out.println("Creation du projet et ajout des taches 1 à 6");
-        p.rajouterTache(new Tache("Tache 1", "Je dois dormir lundi", 5, 12));
-        p.rajouterTache(new Tache("Tache 2", "Je dois manger", 8, 20));
-        p.rajouterTache(new Tache("Tache 3", "Je dois travailler", 7, 15));
-        p.rajouterTache(new Tache("Tache 4", "Je dois sortir lundi", 2, 28));
-        p.rajouterTache(new Tache("Tache 5", "Je dois me lever", 1, 7));
-        p.rajouterTache(new Tache("Tache 6", "Je dois bouger", 4, 13));
+        p.rajouterTache(new Tache("Tache 1", "Je dois dormir lundi", MAINTENANT-2*JOUR,  MAINTENANT+2*JOUR));
+        p.rajouterTache(new Tache("Tache 2", "Je dois manger",  MAINTENANT-1*JOUR,  MAINTENANT+5*JOUR));
+        p.rajouterTache(new Tache("Tache 3", "Je dois travailler",MAINTENANT-1*JOUR,  MAINTENANT+5*JOUR));
+        p.rajouterTache(new Tache("Tache 4", "Je dois sortir lundi",MAINTENANT-1*JOUR,  MAINTENANT+5*JOUR));
+        p.rajouterTache(new Tache("Tache 5", "Je dois me lever", MAINTENANT-1*JOUR,  MAINTENANT+5*JOUR));
+        p.rajouterTache(new Tache("Tache 6", "Je dois bouger", MAINTENANT-5*JOUR,  MAINTENANT-2*JOUR));
         System.out.println(p.toString());
         
         System.out.println("Arret des taches 1 et 4");
--- src/main/java/Projet/Tache.java
+++ src/main/java/Projet/Tache.java
@@ -5,13 +5,13 @@ public class Tache {
     private String nom, description;
     private long id;
     private static long idGlobal = 0;
-    private int datedbt, datefin;
+    private long datedbt, datefin;
 
     Tache() {
         this("","",0,0);
     }
 
-    Tache(String givenName, String givenDesc, int dated, int datef) {
+    Tache(String givenName, String givenDesc, long dated, long datef) {
         nom = givenName;
         description = givenDesc;
         id = idGlobal;
@@ -40,11 +40,13 @@ public class Tache {
         this.description = description;
     }
 
-    public int getDateFin() {
+    public long getDateFin() {
         return datefin;
     }
         
     public String toString() {
-        return "id: " + id + "  | nom: " + nom + " | datedbt: " + datedbt + " | datefin: " + datefin;
+        long  actuel = System.currentTimeMillis()/1000;
+        double f = 100.0*(actuel-datedbt)/ (datefin-datedbt);
+        return "id: " + id + "  | nom: " + nom + " | datedbt: " + datedbt + " | datefin: " + datefin+"  "+String.valueOf(f).substring(0, Math.min(String.valueOf(f).length(), 4)) +"%";
     }
 }
